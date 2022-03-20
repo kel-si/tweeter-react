abc65ac6f65f106a09de63abd9f919c21702f005
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-19 18:09:28 -0600

    disable form submit

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -3,9 +3,15 @@ function TweetForm() {
 
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form method="post" action="/tweets" className="newtweet__form" onSubmit={(event) => {
+          event.preventDefault()
+        }}
+        >
         <textarea className="form__textarea" name="text" placeholder={message}></textarea>
-        <input type="submit" value="Tweet" className="form__input"/>
+        <input 
+        type="submit" value="Tweet" className="form__input" onClick={(event) => {
+          event.preventDefault()
+        }}/>
         <span className="form__counter">140</span>
       </form>
     </section>
